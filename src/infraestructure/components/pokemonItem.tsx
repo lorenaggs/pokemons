6d1069024cdad2c2f8e96f24757381c1c06a30f1
Lorena Guartazaca <lorenaggs2812@gmail.com>
2022-08-22 07:58:02 -0500

    fix bug

--- src/infraestructure/components/pokemonItem.tsx
+++ src/infraestructure/components/pokemonItem.tsx
@@ -3,8 +3,7 @@ import PokemonItemBack from "./pokemonItemBack";
 import {useParams} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect, useState} from "react";
-import {PokemonsData} from "../../domain/models";
-import {getPokemons, getPokemonSelected} from "../../domain/store/slices/pokemon";
+import { getPokemonSelected} from "../../domain/store/slices/pokemon";
 import {Type} from "../../domain/models/PokemonInformation";
 import RadarChart from "./RadarChart";
 
@@ -12,41 +11,47 @@ import RadarChart from "./RadarChart";
 const PokemonItem = () => {
     const dispatch = useDispatch();
 
-    const [pokemon, setPokemon] = useState({});
     let { id } = useParams();
     const {isLoading,  pokemonSelected } = useSelector( (state:any) => state.pokemons )
 
     useEffect(()=>{
+        debugger
+        console.log(pokemonSelected)
         // @ts-ignore
         dispatch(getPokemonSelected(id))
     },[])
 
 
+    const printCard = ()=>{
+        return (
+            <div className='cardItem'>
+                <div className='detail'>
+                    <img className='imageItem'
+                         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+                         alt="ImagePrin"/>
+                    <section className='detailItem'>Detalles del pokemon
+                        <h3>{pokemonSelected.name}</h3>
+                        <p>Numero: #{id}</p>
+                        <p>Altura: {pokemonSelected.height}</p>
+                        <p>Tipo {
+                            pokemonSelected.types && pokemonSelected.types.map((x:Type) => (
+                                <p>- {x.type.name}</p>
+                            ))
+                        }</p>
+                        <p>Estadisticas base</p>
+                        <section>
+                            {pokemonSelected.stats && <RadarChart data={pokemonSelected.stats}/>}
+                        </section>
+                    </section>
+                </div>
+            </div>
+        );
+    }
     return (
      <>
-        <PokemonItemBack/>
-         {!isLoading && <div className='cardItem'>
-         <div className='detail'>
-             <img className='imageItem'
-                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
-                  alt="ImagePrin"/>
-             <section className='detailItem'>Detalles del pokemon
-                 <h3>{pokemonSelected.name}</h3>
-                 <p>Numero: #{id}</p>
-                 <p>Altura: {pokemonSelected.height}</p>
-                 <p>Tipo {
-                    pokemonSelected?.types.map((x:Type) => (
-                        <p>- {x.type.name}</p>
-                    ))
-                  }</p>
-                 <p>Estadisticas base</p>
-                 <section>
-                     <RadarChart data={pokemonSelected?.stats} />
-                 </section>
-             </section>
-         </div>
-     </div>}
-         </>
+         <PokemonItemBack/>
+        {!isLoading && printCard()}
+     </>
     )
 }
 export default PokemonItem;
